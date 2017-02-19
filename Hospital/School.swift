0560355c0b09a6cb3aa80d461177ebc77f07c69a
othmansafarini <othman_safarini@live.co.uk>
2017-02-19 16:24:26 +0400

    It was easy

--- Hospital/School.swift
+++ Hospital/School.swift
@@ -2,11 +2,88 @@
 //  School.swift
 //  Hospital
 
+protocol Payable {
+    func wages() -> Double
+}
 
+extension Payable {
+    func wages() -> Double {
+        return 50_000.00
+    }
+}
 
+protocol TimeOff {
+    var vacationDays: Int { get set}
+    func requestForVacation(_ days: Int) -> Bool
+}
 
+extension TimeOff {
+    func requestForVacation(_ days: Int) -> Bool {
+        
+        if days <= vacationDays {
+            return true
+        }
+        return false
+    }
+}
 
+enum Punishment {
+    case severe, light, medium
+}
 
+protocol Reprimand {
+    func disciplineStudent(_ scale: Punishment) -> String
+    func callParentDeliveringMessage(_ scale: Punishment) -> String
+}
+
+extension Reprimand {
+    func disciplineStudent(_ scale: Punishment) -> String {
+        
+        if scale == .severe {
+            return "We are expelling you from the school!"
+        } else if scale == .light {
+            return  "You're to go back to class, don't do it again."
+        } else {
+            return  "Why would you think that's a good idea?! You're going to detention."
+        }
+    }
+    
+    func callParentDeliveringMessage(_ scale: Punishment) -> String {
+        
+        if scale == .severe {
+            return "Your child has been expelled from the school."
+        } else if scale == .light {
+            return "Your child thought it was funny to put gum in Amy's hair."
+        } else {
+            return "Your child has become a terror in the classroom."
+        }
+    }
+}
+
+enum Subject {
+    case math, science, english
+}
+
+protocol Teach {
+    func teachSubject(_ subject: Subject) -> String
+}
+
+extension Teach {
+    func teachSubject(_ subject: Subject) -> String {
+        switch subject {
+        case .english:
+            return "To read or not to read. Everyone take out your english books."
+        case .math:
+            return "Take out your math books please."
+        case .science:
+                return "Time to learn the best subject of all! Science!!"
+        default:
+            return "Time to learn the best subject of all! Science!!"
+        }
+    }
+}
+
+protocol Employee: Payable, TimeOff { }
 
 
 
--- Hospital/SchoolEmployees.swift
+++ Hospital/SchoolEmployees.swift
@@ -2,8 +2,39 @@
 //  SchoolEmployees.swift
 //  Hospital
 
+struct AdvisoryBoardMember {
+    var name: String
+    var vacationDays = 30
+    
+    func wages() -> Double {
+        return 100_000.00
+    }
+}
 
+extension AdvisoryBoardMember: Employee {
+}
 
+struct Principal {
+    var name: String
+    var vacationDays: Int = 20
+    
+    func wages() -> Double {
+        return 80_000.00
+    }
+}
+
+extension Principal: Employee, Reprimand {
+    
+}
+
+struct Teacher {
+    var name: String
+    var vacationDays: Int
+}
+
+extension Teacher: Employee, Teach {
+    
+}
 
 
 
